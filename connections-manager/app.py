486a4ba56f3c2c7f8b8180670d1bc94e0483927b
chirag <chiraggupta148179@gmail.com>
2022-09-06 15:10:25 +0530

    initial commit

---
+++ connections-manager/app.py
@@ -0,0 +1,19 @@
+from decouple import config
+from binance import ThreadedWebsocketManager
+
+api_key = config("API_KEY")
+api_secret = config("SECRET_KEY")
+
+def main():
+    symbol = "ETHBTC"
+    twm = ThreadedWebsocketManager(api_key=api_key, api_secret=api_secret)
+    twm.start()
+
+    def handle_message(msg):
+        print(msg)
+
+    twm.start_trade_socket(callback=handle_message,symbol=symbol)
+    twm.join()
+
+if __name__ == "__main__":
+    main()
